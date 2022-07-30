4b1796b8ffc6329b5298374b3eb56c96a145b480
liudong <liudong@pfdm.cn>
2022-07-30 11:38:18 +0800

    modify libmetartccore, build success

--- include/yang_config.h
+++ include/yang_config.h
@@ -38,7 +38,7 @@
 #define Yang_HaveDtls 1 //default:using dtls
 #define Yang_HaveDatachannel 1
 
-#define Yang_Using_TWCC 1
+#define Yang_Using_TWCC 0
 #define Yang_Rtp_I_Withmeta 1
 #define Yang_CTimer_Epoll_Timeout 200
 #endif /* INCLUDE_YANG_CONFIG_H_ */
